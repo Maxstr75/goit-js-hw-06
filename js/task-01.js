8ce1a5d30536cfeb3fda81b2771d493c22c83d54
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-04-21 22:08:30 +0300

    add task 1 pravki

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,9 @@ const list = document.querySelectorAll('li.item');
 console.log(`В списке ${list.length} категории.`);
 
 const ul = document.querySelectorAll('#categories>li');
-ul.forEach(el => {
 
-  console.log(
-    `Категория: ${el.firstElementChild.textContent}, Количество элементов: ${el.lastElementChild.children.length}`,
-  );
+ul.forEach(el => {
+    console.log(`Категория: ${el.firstElementChild.textContent},
+    Количество элементов: ${el.lastElementChild.children.length}`);
 });
+
