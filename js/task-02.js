80e06db4929383467ee0d3f3fa32232f365aa665
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-04-22 21:51:40 +0300

    add task 2 pravki

--- js/task-02.js
+++ js/task-02.js
@@ -7,8 +7,18 @@ const ingredients = [
   'Condiments',
 ];
 
-const ulEl = document.querySelector('#ingredients');
+// const idEl = document.querySelector('#ingredients');
+// console.log(idEl);
 
-const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+// const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
 
-ulEl.innerHTML = list;
+// ulEl.innerHTML = list;
+
+const idEl = document.querySelector('#ingredients');
+console.log(idEl);
+
+const elements = ingredients.map(options => {
+  const ingredientEl = createElement('li');
+  ingredientEl.textContent = 'option';
+  idEl.append(elements);
+});
