73bfbc5527fcc4205753546fb8b05cade9d62279
Maxstr75 <91918033+Maxstr75@users.noreply.github.com>
2022-04-25 15:13:34 +0300

    add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -6,14 +6,19 @@
 // </div> */}
 
 
+const changeColorBtn = document.querySelector('button.change-color');
+changeColorBtn.addEventListener('click', handleChangeColorBtn);
 
-const randomIntegerFromInterval = () => {
-  return Math.floor(Math.random() *  16777215);
+function handleChangeColorBtn () {
+  const body = document.querySelector('body');
+  const spanColorText = document.querySelector('span.color');
+
+  body.style.backgroundColor = getRandomHexColor();
+  spanColorText.textContent = getRandomHexColor();
 };
-
-
+  
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
